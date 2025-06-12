eab2ac8c676110d8b305d9b91489d4dc0c964ebd
claudioDang47 <claudiodangiolo@gmail.com>
2025-06-12 15:37:05 -0300

    Se agrego el paquete dotenv, también se genero el comando dev y start

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
+require('dotenv').config();
 const express = require('express');
-
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
-console.log('Funciona de puta madre')
 
 app.get('/', (req, res) => {
     res.send(`<h1>Bienvenido al curso back-end con Express</h1>
         <p>El servidor esta corriendo en el puerto ${PORT}</p>`)
 } )
 
-app.listen(PORT, () => console.log(`App funcionando en puerto ${PORT}`))
+app.listen(PORT, () => console.log(`Servidor: http://localhost:${PORT}`))
